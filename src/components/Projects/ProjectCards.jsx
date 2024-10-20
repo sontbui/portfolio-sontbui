5cd1585b8caa95eec77dba12804e3a240e12b2ed
Bui Thanh Son <sonbuithanh306@gmail.com>
2024-10-21 01:46:09 +0700

    have bug in there

--- src/components/Projects/ProjectCards.jsx
+++ src/components/Projects/ProjectCards.jsx
@@ -3,36 +3,49 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import { Typography, Box } from "@mui/material";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" width={'100%'} height={'300px'} />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>
+          <Typography sx={{ fontFamily: 'Orbitron' }}>
+            <strong className="purple">{props.title}</strong>
+          </Typography>
+        </Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+          <Typography sx={{ fontFamily: 'Orbitron' }}>
+            {props.description}
+          </Typography>
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        <Box  display="flex" 
+        justifyContent="center" 
+        style={{ padding: '10px' }}>
+          <Button variant="primary" href={props.ghLink} target="_blank" style={{ marginTop: '10px' }}>
+            <Typography sx={{ fontFamily: 'Orbitron' }}>
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Typography>
           </Button>
-        )}
+         
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "15px", marginTop: '10px' }}
+            >
+              <Typography sx={{ fontFamily: 'Orbitron' }}>
+                <CgWebsite /> &nbsp;
+                {"Demo"}
+              </Typography>
+            </Button>
+          )}
+        </Box>
+        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
       </Card.Body>
     </Card>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import ecommerce from "../../Assets/Projects/ecommerceWeb.jpg";
+import chatBotApp from "../../Assets/Projects/chatBotApp.jpg";
+import foodModel from "../../Assets/Projects/foodModel.png";
+import questionModel from "../../Assets/Projects/questionModel.png";
+import quizzApp from "../../Assets/Projects/quizzApp.jpg";
 import { Typography } from "@mui/material";
 
 function Projects() {
@@ -16,79 +16,68 @@ function Projects() {
       <Particle />
       <Container>
         <Typography className="project-heading" style={{ fontFamily: "Orbitron" }}>
-          My Recent <strong className="purple">Works </strong>
+          My  <strong className="purple">Projects </strong>
         </Typography>
-        <Typography style={{ fontFamily: "Orbitron", color:'white', marginTop:'20px' }}>
+        <Typography style={{ fontFamily: "Orbitron", color: 'white', marginTop: '20px' }}>
           Here are a few projects I've worked on recently.
         </Typography>
 
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={ecommerce}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="E-commerce-Platform"
+              description="Users can create accounts and make online purchases. The system allows users to browse products, add them to their cart, and check out easily.The admin will monitor and process orders, ensuring smooth delivery. It utilizes Apache Kafka for data management and message transmission between services, while Zookeeper ensures configuration management and the state of the distributed system."
+              ghLink="https://github.com/sontbui/E-commerce-Platform.git"
+            // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={chatBotApp}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Craft Social Media"
+              description="Develop a Flutter application that integrates social media features, enabling users to build networks easily. The app will analyze user behavior for personalized recommendations. Users can create profiles, connect with friends, share content, and interact through comments and messages. Manage user data and posts efficiently using Firebase, with Firebase Authentication for secure sign-up and login.."
+              ghLink="https://github.com/sontbui/Craft-Social-Open-Last.git"
+            // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+          {/* none link github */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={foodModel}
+              isBlog={false}
+              title="Food Recognition Pre-Train"
+              description="Utilize pre-existing models like YOLO v8, YOLO v9, and CNN to retrain them with your custom dataset for improved food recognition. This process enhances the models' ability to accurately identify and classify various food items, leveraging established frameworks to achieve better performance. By fine-tuning these models, you can adapt them to recognize specific food categories relevant to your application, ensuring higher accuracy and efficiency."
+              ghLink="/"
+            // demoLink="https://editor.soumya-jit.tech/"
+            />
+          </Col>
+          {/* none link github */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={questionModel}
+              isBlog={false}
+              title="Question Answering Pre-Train"
+              description="Leverage existing models to retrain a question-and-answer dataset for developing a model capable of responding to user inquiries. The models employed include BERT, PhoBERT, and ViderBerta-xsmall. The training process will involve fine-tuning these models to enhance their ability to accurately answer questions in Vietnamese. By utilizing these robust architectures, the system aims to improve the quality of responses and provide users with reliable information efficiently."
+              ghLink="/"
+            // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={quizzApp}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Quizz App Simple"
+              description="Develop a self-learning app for reviewing Object-Oriented Programming (OOP) topics. Utilize Flutter to create interactive quizzes that help users reinforce their understanding of key concepts. The app will feature various question types, such as multiple choice and true/false, allowing users to engage with the material effectively. Additionally, users can track their progress and receive instant feedback, making learning both fun and informative."
+              ghLink="https://github.com/sontbui/QuizAppFlutterFinal.git"
             // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
